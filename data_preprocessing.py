8307fe3c49cae8ae1bbf9436f79074ee027dbcb0
AryushG <aghildiyal@student.ethz.ch>
2026-03-01 19:44:40 +0100

    added manual selection of sheets

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -11,6 +11,41 @@ file_path = filedialog.askopenfilename(initialdir = "01-INPUT-DATA",
                                           title = "Select Input Data",
                                           filetypes = [("Excel files", ".xlsx .xls")])
 # ==========================================================
+# Select sheets from excel file
+# ==========================================================
+def select_sheets():
+
+    def selection_list():
+        for var in vars:
+            if var.get() == 1:
+                selection.append(sheet_list[vars.index(var)])
+
+    df = pd.read_excel(file_path,sheet_name = None)
+    sheet_list = list(df.keys())
+    selection = []
+    root = tk.Tk()
+    root.title("Select Trafo Data Sheet(s)")
+    root.geometry = ('100x100')
+
+    vars = [tk.IntVar() for sheet in sheet_list]
+    btns = [tk.Checkbutton(root, text = sheet, variable = vars[sheet_list.index(sheet)],
+                        onvalue=1, offvalue=0) for sheet in sheet_list]
+
+    for btn in btns:
+        btn.pack(side = 'top')
+
+    sbtn = tk.Button(root, text = 'Apply', 
+                        command = selection_list) 
+    sbtn.pack(side = 'top')      
+
+    xbtn = tk.Button(root, text = 'Exit', 
+                        command = root.destroy) 
+    xbtn.pack(side = 'top')      
+    root.mainloop()
+
+    return(selection)
+
+# ==========================================================
 # Generic trafo loader
 # ==========================================================
 def load_trafo(sheet_name):
@@ -138,48 +173,11 @@ def convert_to_15min(df, column_name):
 # ==========================================================
 
 ##TEST##
-""" import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
 
-if __name__ == "__main__":
+selection = select_sheets()
 
-    df = load_grid_exchange()
-    grid = convert_to_15min(df,"trafo1_kW")
+print(selection)
 
-
-    start_date = "2024-02-10"
-    end_date   = "2024-02-11"
-
-    data = grid[
-        (grid["timestamp"] >= start_date) &
-        (grid["timestamp"] < end_date)
-    ].copy()
-
-    plt.figure()
-
-    plt.plot(data["timestamp"], data["trafo1_kW"])
- #   plt.plot(data["timestamp"], data["trafo2_kW"])
- #   plt.plot(data["timestamp"], data["grid_exchange_kW"])
-
-    # Dynamic axis formatting
-    if (pd.to_datetime(end_date) - pd.to_datetime(start_date)).days <= 1:
-        # Single day
-        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
-        plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=1))
-    else:
-        # Multi-day
-        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d-%m'))
-        plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=1))
-
-    plt.xticks(rotation=45)
-
-    plt.xlabel("Time")
-    plt.ylabel("Power (kW)")
-#    plt.title(f"Trafo1, Trafo2 & Grid Exchange\n{start_date} to {end_date}")
-#    plt.legend(["Trafo1", "Trafo2", "Grid Exchange"])
-
-    plt.tight_layout()
-    plt.show() """
 ##TEST##
 '''
 if __name__ == "__main__":
@@ -216,7 +214,7 @@ if __name__ == "__main__":
 # ==========================================================
 # Visualizing the data
 # ==========================================================
-import matplotlib.pyplot as plt
+""" import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 if __name__ == "__main__":
@@ -255,4 +253,4 @@ if __name__ == "__main__":
     plt.legend(["Trafo1", "Trafo2", "Grid Exchange"])
 
     plt.tight_layout()
-    plt.show()
+    plt.show() """
